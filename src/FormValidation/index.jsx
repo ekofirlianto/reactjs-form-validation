24f914fb7833f868b613b61b2bfe0a3a9f61c2fa
Eko Firlianto <eko.frilianto@gmail.com>
2022-09-14 14:57:00 +0700

    ready push to heroku

---
+++ src/FormValidation/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import FormElement from './DetailForm/FormElement';
+
+export default class FormValidation extends React.Component {
+  render() {
+    return (
+      <div>
+        <FormElement />
+      </div>
+    );
+  }
+}
